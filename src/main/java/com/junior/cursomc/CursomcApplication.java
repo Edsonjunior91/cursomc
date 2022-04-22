b47b593ceb11d2f4fc76f8a371a667668ca52440
Edson Junior <edson_junior91@hotmail.com>
2022-04-21 23:17:44 -0300

    Projeto Criado

---
+++ src/main/java/com/junior/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.junior.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
